9e736c48255fb036aaa0a8d7c19dda340b054ea9
Håvard Wanvik Stenersen <haavaws@gmail.com>
2018-03-26 23:53:50 +0200

    Changed data fields of the routing table and distance table entry structs from char to uint8_t.

--- Hjemmeeksamen/routing_daemon.h
+++ Hjemmeeksamen/routing_daemon.h
@@ -1,16 +1,20 @@
+#include <time.h>
+#include <inttypes.h>
+
 #define TTL 15 /* Max TTL of any MIP packet */
 #define MAX_EVENTS 1
 #define MAX_MIP 256
 #define UPDATE_WAIT 30 /* Max time to wait between routing updates */
 
 struct routing_table_entry{
-  char next_hop;
-  char dest_mip;
-  char cost;
+  uint8_t next_hop;
+  uint8_t dest_mip;
+  uint8_t cost;
 } __attribute__((packed));
 
 struct distance_table_entry{
-  char dest_mip;
-  char *next_hop;
-  char *cost;
+  uint8_t dest_mip;
+  uint8_t *next_hop;
+  uint8_t *cost;
+  time_t *timestamp;
 };
