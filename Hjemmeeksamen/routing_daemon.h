07d56bdb1c0fc54627d55a74979909fa446d2526
Håvard Wanvik Stenersen <haavaws@gmail.com>
2018-04-03 17:23:19 +0200

    Added global values for an invalid MIP address, broadcast MIP address, cost for an unreachable destination and a number of times to wait before invalidating an entry.

--- Hjemmeeksamen/routing_daemon.h
+++ Hjemmeeksamen/routing_daemon.h
@@ -2,9 +2,15 @@
 #include <inttypes.h>
 
 #define TTL 15 /* Max TTL of any MIP packet */
+#define UNREACHABLE 16 /* Cost indicating unreachable destination */
 #define MAX_EVENTS 1
 #define MAX_MIP 256
-#define UPDATE_WAIT 30 /* Max time to wait between routing updates */
+#define BROADCAST_MIP 255 /* Broadcast MIP address */
+#define BAD_MIP 255 /* Value indicating an invalid MIP address */
+#define UPDATE_WAIT 30 /* Max seconds to wait between routing updates */
+/* Number of times to wait before invalidating an entry in the distance
+ * table */
+#define WAIT_NUM_TTL 6
 
 struct routing_table_entry{
   uint8_t next_hop;
