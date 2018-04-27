1f6376748ad54ac32dbad08bdf6b3bbec1c818a1
Håvard Wanvik Stenersen <haavaws@gmail.com>
2018-04-27 04:04:38 +0200

    Added documentation. Added some debugging. Removed some superfluous arguments in function calls.

--- Hjemmeeksamen 2/transport_daemon.c
+++ Hjemmeeksamen 2/transport_daemon.c
@@ -12,27 +12,35 @@
 
 
 int main(int argc, char* argv[]){
+  /* Structure for containing incoming remote connection sessions */
   struct connection_data *conn_data = (struct connection_data *)
       malloc(sizeof(struct connection_data));
+  /* Structure for containing data about local connections, and outgoing
+   * connection sessions */
   struct socket_container *socks = (struct socket_container *)
       malloc(sizeof(struct socket_container));
 
+  /* Epoll data */
   int epfd,nfds;
   struct epoll_event events[MAX_EVENTS];
 
+  /* Socket paths */
   char *app_name;
   char *mip_name;
 
+  /* Timeout before resending un-acked packets */
   int timeout;
+  /* Indicates if debug information should be logged to console */
   int debug = 0;
 
   int i;
   ssize_t ret;
-  time_t now;
+  time_t now; /* Current time */
 
-  printf("%d\n",argc);
+
 
   /* Argument handling */
+
   if(argc > 1){
     /* Print help */
     if(strcmp(argv[1],"-h") == 0){
@@ -41,7 +49,7 @@ int main(int argc, char* argv[]){
     }
   }
 
-  if(argc<4){
+  if(argc < 4){
     /* Not enough arguments */
     print_help(argv[0]);
     exit(EXIT_FAILURE);
@@ -65,6 +73,7 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
+  /* Socket paths */
   mip_name = argv[2 + debug];
   app_name = argv[3 + debug];
 
@@ -86,6 +95,11 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
+  if(debug){
+    fprintf(stdout, "Listening for incoming connections from applications on "
+        "path \"%s\"\n", app_name);
+  }
+
   /* Connect to the MIP daemon */
   socks->mip = connect_socket(mip_name);
 
@@ -100,7 +114,11 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
-  /* Signal handler */
+  if(debug){
+    fprintf(stdout, "Connected to MIP daemon on path \"%s\"\n", mip_name);
+  }
+
+  /* Signal handler. Capturing keyboard interrupts: Ctrl-C and Ctrl-\ */
   socks->signal = setup_signal_fd();
 
   if(socks->signal == -1){
@@ -109,7 +127,7 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
-  /* Epoll instance */
+  /* Create the epoll instance */
   epfd = create_epoll_instance(socks);
 
   if (epfd < 0){
@@ -120,10 +138,19 @@ int main(int argc, char* argv[]){
 
   socks->epfd = epfd;
 
+  if(debug){
+    fprintf(stdout, "Waiting for incoming data from applications and MIP "
+        "daemon.\n");
+  }
 
-  /* Handle events in the epoll instance */
+
+
+  /* MAIN LOOP */
+  /* Perform the transport layer functionality */
   for(;;){
     now = time(NULL);
+
+    /* Wait for events, non-blocking */
     nfds = epoll_wait(epfd, events, MAX_EVENTS, 0);
 
     if(nfds == -1){
@@ -133,27 +160,37 @@ int main(int argc, char* argv[]){
       exit(EXIT_FAILURE);
     }
 
-    if(nfds == 0){
-      /* Check for timeouts */
-      for(i = 0; i < socks->num_apps; i++){
-        if(socks->app_conns[i].num_ack_queue > 0){
-          if(now - socks->app_conns[i].packet_timestamp[0] > timeout){
-            /* Resend the packets if there was a timeout */
-            if(resend_packets(&socks->app_conns[i], socks) == -1){
-              perror("main: resend_packets()");
-              close_sockets(socks,0);
-              free_conn_data(conn_data);
-              exit(EXIT_FAILURE);
-            }
+    /* Check if any of the packets awaiting acks have timed out */
+    /* Iterate through all connected applications */
+    for(i = 0; i < socks->num_apps; i++){
+      /* If they have any packets awaiting acks */
+      if(socks->app_conns[i].num_ack_queue > 0){
+        /* Check if the first packet has timed out */
+        if(now - socks->app_conns[i].packet_timestamp[0] > timeout){
+
+          if(debug){
+            fprintf(stdout, "Packet meant for MIP %d on port %d has timed "
+                "out while waiting for ack.\n", socks->app_conns[i].mip,
+                socks->app_conns[i].port);
+            fprintf(stdout, "Resending all unacked packets.\n");
+          }
+
+          /* Resend the packets if there was a timeout */
+          if(resend_packets(&socks->app_conns[i], socks) == -1){
+            perror("main: resend_packets()");
+            close_sockets(socks,0);
+            free_conn_data(conn_data);
+            exit(EXIT_FAILURE);
           }
         }
       }
     }
 
+
     /* Iterate through the triggered events */
     for (i = 0; i < nfds; i++){
 
-      /* A keyboard interrupt was signaled */
+      /* A keyboard interrupt was signaled, shutdown the transport daemon */
       if(events[i].data.fd == socks->signal){
         if(keyboard_signal(events[i].data.fd) == 0){
           close_sockets(socks, 1);
@@ -163,9 +200,14 @@ int main(int argc, char* argv[]){
       }
 
 
+      /* A new application connected to the MIP daemon */
       else if(events[i].data.fd == socks->app){
+        if(debug){
+          fprintf(stdout, "\n");
+        }
         struct conn_app new_conn = { 0 };
 
+        /* Accept the new application */
         new_conn.sock = accept(socks->app, NULL, NULL);
 
         if(new_conn.sock == -1){
@@ -175,7 +217,8 @@ int main(int argc, char* argv[]){
           exit(EXIT_FAILURE);
         }
 
-        ret = init_app(&new_conn, socks, epfd);
+        /* Initialize the connection data */
+        ret = init_app(&new_conn, epfd);
 
         if(ret == -1){
           perror("main: init_app()");
@@ -186,11 +229,13 @@ int main(int argc, char* argv[]){
           /* Communication error, disconnect */
           if(debug){
             fprintf(stdout, "Communication error with transport application, "
-                "connection closed.\n");
+                "connection closed.\n\n");
           }
           continue;
         }
 
+        /* Add the connection to the structure containing data about local
+         * connections */
         socks->app_conns = (struct conn_app *) realloc(socks->app_conns,
             sizeof(struct conn_app) * (socks->num_apps + 1));
         socks->app_conns[socks->num_apps] = new_conn;
@@ -203,8 +248,17 @@ int main(int argc, char* argv[]){
 
       }
 
+
+
+      /* Incoming data from the MIP daemon */
       else if(events[i].data.fd == socks->mip){
-        ret = recv_transport_packet(socks, conn_data);
+
+        if(debug){
+          fprintf(stdout, "\n");
+        }
+
+        /* Handle the data */
+        ret = recv_transport_packet(socks, conn_data, debug);
 
         if(ret == -1){
           perror("main: recv_transport_packet()");
@@ -213,20 +267,35 @@ int main(int argc, char* argv[]){
           exit(EXIT_FAILURE);
         }else if(ret == -2){
           /* MIP daemon has performed an orderly shutdown, quit */
+
           if(debug){
             fprintf(stdout, "MIP daemon has performed an orderly shutdown, "
                 "shutting down.\n");
           }
+
           close_sockets(socks,1);
           free_conn_data(conn_data);
           exit(EXIT_FAILURE);
         }
 
+        if(debug){
+          fprintf(stdout, "\n");
+        }
+
       }
 
+
+
+      /* Incoming data from a connected application */
       else if(is_conn(events[i].data.fd, socks->app_conns, socks->num_apps)
           != -1){
-        ret = recv_from_app(events[i].data.fd, socks, conn_data, debug);
+
+        if(debug){
+          fprintf(stdout, "\n");
+        }
+
+        /* Handle the data */
+        ret = recv_from_app(events[i].data.fd, events[i].events, socks, debug);
         if(ret == -1){
           perror("main: recv_from_app()");
           close_sockets(socks, 0);
@@ -234,10 +303,11 @@ int main(int argc, char* argv[]){
           exit(EXIT_FAILURE);
         }
 
+        if(debug){
+          fprintf(stdout, "\n");
+        }
+
       }
-
-    }
-
-  }
-
-}
+    }/* END EVENT HANDLING */
+  }/* END MAIN LOOP */
+}/* END MAIN FUNCTION */
