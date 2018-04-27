c2e27f7d8fcdb7784f110d9ba83a8c8775ee00b9
Håvard Wanvik Stenersen <haavaws@gmail.com>
2018-04-27 04:05:31 +0200

    Added documentation. Removed some superfluous arguments from function calls.

--- Hjemmeeksamen 2/transport_daemon.h
+++ Hjemmeeksamen 2/transport_daemon.h
@@ -11,39 +11,52 @@
 #define SEQ_NUM_MAX 65535 /* The largest possible sequence number */
 
 struct transport_packet{
+  /* The two first bits of the pad_and_port array are for the padding length,
+   * while the remaining 14 are for the port */
   uint8_t pad_and_port[2];
-  uint16_t seq_num;
-  uint8_t payload[];
+  uint16_t seq_num; /* Sequence number stored in network-byte-order */
+  uint8_t payload[]; /* Payload of the packet, including padding */
 } __attribute__((packed));
 
+/* For a connected server application, only the mip, sock and port fields are
+ * used */
 struct conn_app{
-  int sock;
+  int sock; /* Socket the application is conneceted to */
+  /* MIP address the application is sending to (invalid, i.e. 255, if the
+   * application is a server) */
   uint8_t mip;
-  uint16_t port;
+  uint16_t port; /* Port communicated on */
+  /* Sequence number of last packet sent (initialized to SEQ_NUM_MAX) */
   uint16_t seq_num;
+  /* Total amount of bytes of payload received by the intended recipient */
   ssize_t payload_bytes_sent;
-  struct transport_packet *sent_packets[10];
-  int packet_size[10];
+  struct transport_packet *sent_packets[10]; /* Packet window */
+  int packet_size[10]; /* Size of the packets in the packet window */
+  int packet_pad[10]; /* Padding length of the packets in the packet window */
+  /* Time the packets in the packet window were sent */
   time_t packet_timestamp[10];
-  int num_ack_queue;
+  int num_ack_queue; /* Number of packets awaiting ack */
 };
 
+/* Remote connection sessions */
 struct connection_data{
-  uint16_t *seq_nums;
-  uint16_t *port_conns;
-  uint8_t *mip_conns;
-  int num_sessions;
+  uint16_t *seq_nums; /* Sequence numbers of the next expected packets */
+  uint16_t *port_conns; /* Ports that are being communicated on */
+  uint8_t *mip_conns; /* Source MIP address of the sessions */
+  int num_sessions; /* Number of sessions */
 };
 
+/* Local connection data */
 struct socket_container{
-  int epfd;
-  int app;
-  int mip;
-  struct conn_app *app_conns;
-  int num_apps;
-  int signal;
+  int epfd; /* Epoll instance */
+  int app; /* Socket listening for application connections */
+  int mip; /* Socket connected to the MIP daemon */
+  struct conn_app *app_conns; /* Connected applications */
+  int num_apps; /* Number of connected applications */
+  int signal; /* Signal handler */
 };
 
+/* Functions defined in transport.c */
 int setup_listen_socket(char* un_sock_name);
 void print_help(char *file_name);
 void close_sockets(struct socket_container *socks, int unlink_mip_path);
@@ -54,9 +67,8 @@ void free_conn_data(struct connection_data *conn_data);
 int keyboard_signal(int signal_fd);
 int is_conn(int sock, struct conn_app *app_conns, int sock_len);
 int recv_transport_packet(struct socket_container *socks,
-    struct connection_data *conn_data);
-int resend_packets(struct conn_app *app_conn, struct socket_container *socks);
-int recv_from_app(int app_sock, struct socket_container *socks,
     struct connection_data *conn_data, int debug);
-int init_app(struct conn_app *app_conn, struct socket_container *socks,
-    int epfd);
+int resend_packets(struct conn_app *app_conn, struct socket_container *socks);
+int recv_from_app(int app_sock, uint32_t epoll_events,
+    struct socket_container *socks, int debug);
+int init_app(struct conn_app *app_conn, int epfd);
